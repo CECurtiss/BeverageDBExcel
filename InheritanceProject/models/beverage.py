6fcac9ea4634b9b3bb32caa010966a12073fae05
CECurtiss <curtissclinton@gmail.com>
2025-12-12 17:37:33 -0600

    added price property to beverage class and extended through others. Added price fields to all tables

--- InheritanceProject/models/beverage.py
+++ InheritanceProject/models/beverage.py
@@ -1,15 +1,16 @@
 #Beverage class, Alcohol class, and Beer class go here( maybe also IPA for another?)
 class Beverage:
-    def __init__(self, name, volume_oz):
+    def __init__(self, name, volume_oz, price):
         self.name = name
         self.volume_oz = volume_oz
+        self.price = price
 
     def serve(self):
         return f"Serving {self.volume_oz}oz of {self.name}."
 
 class Alcohol(Beverage):
-    def __init__(self, name, volume_oz, alcohol_content):
-        super().__init__(name, volume_oz)
+    def __init__(self, name, volume_oz, price, alcohol_content):
+        super().__init__(name, volume_oz, price)
         self.alcohol_content = alcohol_content
 
     def serve(self):
@@ -18,8 +19,8 @@ class Alcohol(Beverage):
     
     
 class Beer(Alcohol):
-    def __init__(self, name, volume_oz, alcohol_content, beer_type):
-        super().__init__(name, volume_oz, alcohol_content)
+    def __init__(self, name, volume_oz, price, alcohol_content, beer_type):
+        super().__init__(name, volume_oz, price, alcohol_content)
         self.beer_type = beer_type
 
     def serve(self):
@@ -27,8 +28,8 @@ class Beer(Alcohol):
         return f"Beer is tasty! Enjoy your {self.beer_type}!"
   
 class IPA(Beer):
-    def __init__(self, name, volume_oz, alcohol_content, ibu):
-        super().__init__(name, volume_oz, alcohol_content, beer_type="IPA")
+    def __init__(self, name, volume_oz, price,alcohol_content, ibu):
+        super().__init__(name, volume_oz, price, alcohol_content, beer_type="IPA")
         self.ibu = ibu
 
     def serve(self):
@@ -36,8 +37,8 @@ class IPA(Beer):
         return f"This IPA has a bitterness of {self.ibu} IBU. Cheers!"
     
 class Wine(Alcohol):
-    def __init__(self, name, volume_oz, alcohol_content, grape_varietal, country):
-        super().__init__(name, volume_oz, alcohol_content)
+    def __init__(self, name, volume_oz, price, alcohol_content, grape_varietal, country):
+        super().__init__(name, volume_oz, price, alcohol_content)
         self.grape_varietal = grape_varietal
         self.country = country
 
--- InheritanceProject/services/db_service.py
+++ InheritanceProject/services/db_service.py
@@ -5,8 +5,16 @@ import pandas as pd
 DB_PATH = "../data/beverages.db"
 
 #initialize the database connection
+def init_db(DB_PATH):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        return conn
+    except sqlite3.Error as e:
+        print(f"Error connecting to database: {e}")
+        return None
+    
 
-#create tables if they do not exist
+#do not create tables. let pandas handle that.
 
 #load data from excel files into the database
 
