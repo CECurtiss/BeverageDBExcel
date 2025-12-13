b5a83784fa751bce71a0fad0b976dcb8478b1001
CECurtiss <curtissclinton@gmail.com>
2025-12-12 22:43:38 -0600

    created path variables in main.py to avoid navigation issues for file sources. updated db_sservice to use path variable. ran main.py for the first time to create the db

--- InheritanceProject/main.py
+++ InheritanceProject/main.py
@@ -2,14 +2,22 @@
 #save data to database
 #generate report using excel writer
 from services.db_service import get_connection
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parent
+data_dir = project_root / "data"
+excel_input_dir = data_dir / "excel_input"
+excel_output_dir = data_dir / "excel_output"
+DB_PATH = data_dir / "beverages.db"
 
 def main():
-    conn = get_connection()
+    conn = get_connection(DB_PATH)
     if conn:
         print("Database connection established.")
-        conn.close()
     else:
         print("Failed to connect to the database.")
+
+print("hello world")
 
 if __name__ == "__main__":
     main()
--- InheritanceProject/services/db_service.py
+++ InheritanceProject/services/db_service.py
@@ -2,12 +2,10 @@
 import sqlite3
 from pathlib import Path
 
-DB_PATH = Path("../data/beverages.db")
-
 #initialize the database connection
-def get_connection():
+def get_connection(db_path: Path):
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = sqlite3.connect(db_path)
         return conn
     except sqlite3.Error as e:
         print(f"Error connecting to database: {e}")
