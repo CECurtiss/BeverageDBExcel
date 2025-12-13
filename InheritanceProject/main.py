fd68e20a6de38870aa8001682da0e788e04434f0
CECurtiss <curtissclinton@gmail.com>
2025-12-12 22:20:26 -0600

    added some main.py function. testing push for google colab

--- InheritanceProject/main.py
+++ InheritanceProject/main.py
@@ -1,4 +1,15 @@
-#from db service import init_db , from excel importer import import_excel
 #initialize database and import excel data
 #save data to database
 #generate report using excel writer
+from services.db_service import get_connection
+
+def main():
+    conn = get_connection()
+    if conn:
+        print("Database connection established.")
+        conn.close()
+    else:
+        print("Failed to connect to the database.")
+
+if __name__ == "__main__":
+    main()
